69962264b3f548f726b240bdcf4d70ad861bd5dd
Kevin MacPherson <kevin.macpherson@gmail.com>
2017-12-17 21:09:34 -0500

    Added comments to javascript code and made changes based on review.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,29 @@
+
+// The make grid function which contains the meat of the code.
 let makeGrid = function() {
-  let gridHeight, gridthWidth, gridMain, gridRow, gridBox;
 
-  gridHeight = $("#gridHeight").val();
-  gridWidth = $("#gridWidth").val();
-  gridMain = $("#gridMain");
+  // Gets the height and width of the grid from the form.
+  let gridHeight = $("#gridHeight").val();
+  let gridWidth = $("#gridWidth").val();
+  // Gets where the grid will be displayed from the document,
+  let gridMain = $("#gridMain");
 
-  $("#errorDiv").remove();
+  $("#errorDiv").remove(); // Remove the error on submition.
+
+  // Checks to make sure that the max is not exceeded.
   if ((gridHeight > 32) || (gridWidth > 32) ) {
-    errorDiv="<div id=\"errorDiv\">Error: Please keep the height and width under 32!</div>";
+    let errorDiv="<div id=\"errorDiv\">Error: Please keep the height and width under 32!</div>";
     $("#displayHeader").after(errorDiv);
   } else {
-    div="<div></div>";
+    let div="<div></div>";
 
-    $(".gridRow").remove();
+    $(".gridRow").remove(); // Remove the grid on resubmition or it will add to current grid.
 
+    // Create the grid by adding the right amout of rows then adding the proper amount of boxes.
     for (let i = 0; i < gridHeight; i++) {
       gridMain.append(div);
     };
-    gridRow = gridMain.children();
+    let gridRow = gridMain.children();
     gridRow.addClass("gridRow");
 
     for (let i = 0; i < gridWidth; i++) {
@@ -27,8 +33,10 @@ let makeGrid = function() {
   }
 };
 
-$("#gridButton").click(makeGrid);
+$("#gridButton").click(makeGrid);  // Method to create the grid on clicking on the submit button.
 
+
+// Creates a listener for the clicks within the area of the grid and changing the colour of the background to the color selected by the colour picker.
 $("#gridMain").on('click', '.gridBox', function(){
   let gridColour = $("#gridColour").val();
   console.log(gridColour);
