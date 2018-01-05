e5829760bffc0faaae31ec7994967a6582abd416
Kevin MacPherson <kevin.macpherson@gmail.com>
2018-01-04 21:06:29 -0500

    Moving from jQuery to Javascript for new course content

--- js/app.js
+++ js/app.js
@@ -1,44 +1,74 @@
 
+//Refactor from previous course using jQuery to standard Javascript.
+
 // The make grid function which contains the meat of the code.
 let makeGrid = function() {
 
   // Gets the height and width of the grid from the form.
-  let gridHeight = $("#gridHeight").val();
-  let gridWidth = $("#gridWidth").val();
+  const gridHeight = document.querySelector('#gridHeight').value;
+  const gridWidth = document.querySelector('#gridWidth').value;
   // Gets where the grid will be displayed from the document,
-  let gridMain = $("#gridMain");
+  const gridMain = document.querySelector('#gridMain');
 
-  $("#errorDiv").remove(); // Remove the error on submition.
+  // Remove the error on submition.
+  const errorDisp = document.querySelector("#errorDiv");
+
+  let gribRow, gridBox;
+
+  if (errorDisp !== null) {
+    errorDisp.remove();
+  }
 
   // Checks to make sure that the max is not exceeded.
   if ((gridHeight > 32) || (gridWidth > 32) ) {
-    let errorDiv="<div id=\"errorDiv\">Error: Please keep the height and width under 32!</div>";
-    $("#displayHeader").after(errorDiv);
+    const errorDiv= document.createElement('div');
+    errorDiv.textContent = "Error: Please keep the height and width under 32!";
+    errorDiv.classList.add("errorDiv");
+    const displayHead = document.querySelector("#displayHeader");
+    displayHead.appendChild(errorDiv);
   } else {
-    let div="<div></div>";
 
-    $(".gridRow").remove(); // Remove the grid on resubmition or it will add to current grid.
+    // Remove the grid on resubmition or it will add to current grid.
+    const gridX = document.querySelector(".gridRow");
+
+    if (gridX !== null) {
+        gridX.remove();
+    }
 
     // Create the grid by adding the right amout of rows then adding the proper amount of boxes.
     for (let i = 0; i < gridHeight; i++) {
-      gridMain.append(div);
+      const div = document.createElement("div");
+      gridMain.appendChild(div);
     };
-    let gridRow = gridMain.children();
-    gridRow.addClass("gridRow");
+    if (gridMain.hasChildNodes()) {
+        gridRow = gridMain.childNodes;
+    }
+    for (let i = 0; i < gridRow.length; i++) {
+      gridRow[i].classList.add("gridRow");
+      for (let j = 0; j < gridWidth; j++) {
+        const div = document.createElement("div");
+        gridRow[i].appendChild(div);
+      };
+      if (gridRow[i].hasChildNodes()) {
+        gridBox = gridRow[i].childNodes;
+      };
+      for (let i = 0; i < gridBox.length; i++) {
+        gridBox[i].classList.add("gridBox");
+      };
+    };
 
-    for (let i = 0; i < gridWidth; i++) {
-      $(".gridRow").append(div);
+    const gridCell = document.querySelectorAll(".gridBox");
+    // Creates a listener for the clicks within the area of the grid and changing the colour of the background to the color selected by the colour picker.
+    for (let i = 0; i < gridCell.length; i++) {
+      gridCell[i].addEventListener('click', function(evt) {
+      const gridColour = document.querySelector('#gridColour').value;
+      evt.target.style.backgroundColor = gridColour;
+      });
     };
-    $(".gridRow").children().addClass("gridBox");
-  }
+  };
 };
 
-$("#gridButton").click(makeGrid);  // Method to create the grid on clicking on the submit button.
+//Gets the button and stores it for later.
+const gridButton = document.querySelector("#gridButton");
 
-
-// Creates a listener for the clicks within the area of the grid and changing the colour of the background to the color selected by the colour picker.
-$("#gridMain").on('click', '.gridBox', function(){
-  let gridColour = $("#gridColour").val();
-  console.log(gridColour);
-  $(this).css("background-color", gridColour);
-});
+gridButton.addEventListener('click', makeGrid);  // Method to create the grid on clicking on the submit button.
